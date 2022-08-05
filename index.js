54efddf384d0b4d18fb7b2fade98e8e75c60639e
Min Yang <evelynym@outlook.com>
2022-08-06 01:43:38 +0930

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello!");
+  res.send("Hello! min");
 });
 
 app.get("/money", function (req, res) {
